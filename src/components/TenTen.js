4cef71f6788c913b0994527a490c97a1a768ca02
foshesss <foshessss@gmail.com>
2022-10-28 21:27:01 -0400

    update1010

--- src/components/TenTen.js
+++ src/components/TenTen.js
@@ -4,7 +4,7 @@ const TenTen = () => (
     <div className="tenten" id='10+10 diagram'>
         <header id='tenten-header' className='problem-header'>
             <h2>10+10 Diagram</h2>
-            <p>Group participants for this activitiy include</p>
+            <p><b>Group participants for this activitiy include</b></p>
             <ul>
                 <li>Médérick Grivel</li>
                 <li>Will Hamilton</li>
@@ -12,10 +12,25 @@ const TenTen = () => (
                 <li>Ethan Klonder</li>
                 <li>K Raj Suql</li>
             </ul>
+            <p>
+                During the design challenge section, our team quickly established our design challenge as finding an efficient utilization of dining hall resources. 
+                Once this was agreed on we could fill in the rest of the chart and brainstorm assumptions, like the fact that certain foods get wasted more than others.
+                <br></br>
+                <br></br>
+                The next section was the initial 10 ideas where we were supposed to think of the most out of the box concepts as possible. Some of these ideas included 
+                displaying wait times outside of the diners so students could see when the best time to eat was and allowing students to eat for free after 9pm. 
+                The idea we decided to expand on was allowing students to take food home since we thought it was the most viable and quickest solution out of the initial 10.
+                <br></br>
+                <br></br>
+                After this, we had to create 10 variations of the idea we chose from the initial 10. Trying to find 10 variations of allowing students to take food home 
+                wasn’t easy but we came up with ideas like sign-ups to get prepackaged meals, allowing staff to eat free at dining halls, and creating a special meal 
+                plan that allows students to take food home.
+            </p>
         </header>
         <img src="https://i.imgur.com/eSW8h27.png" alt="design challenge" />
         <img src="https://i.imgur.com/S3NIYK2.png" alt="first 10" />
         <img src="https://i.imgur.com/09kpBpf.png" alt="second 10" />
+        <img src="https://i.imgur.com/5qHkufU.png" alt="we believe" />
     </div>
 );
 
